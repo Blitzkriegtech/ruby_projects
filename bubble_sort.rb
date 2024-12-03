1ba2fc4e27cce4ce7796f9604d6ce8b99abba2b9
Blitzkriegtech <aljunrendon@yahoo.com>
2024-12-03 19:46:04 +0800

    Edit comment

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -20,33 +20,33 @@ print sort(arr)
 # Output => [1, 2, 3]
 
 
-# #Another approach 
+# Add sort by ascending/descending functionality
 
-# def sort_bubble(array, order: :ascending)
-# array_length = array.size
-# return arry if array_length <= 1
+def sort_bubble(array, order: :ascending)
+array_length = array.size
+return arry if array_length <= 1
 
-# loop do
-# swapped = false
-# (array_length  - 1).times do |i|
-# if order == :ascending
-# if array[i] > array[i + 1]
-# array[i], array[i + 1] = array[i + 1], array[i]
-# swapped = true
-# end
+loop do
+swapped = false
+(array_length  - 1).times do |i|
+if order == :ascending
+if array[i] > array[i + 1]
+array[i], array[i + 1] = array[i + 1], array[i]
+swapped = true
+end
 
-# elsif order == :descending
-# if array[i] < array[i + 1]
-# array[i], array[i + 1] = array[i + 1], array[i]
-# swapped = true
+elsif order == :descending
+if array[i] < array[i + 1]
+array[i], array[i + 1] = array[i + 1], array[i]
+swapped = true
 
-# end
-# end
-# end
-# break unless swapped
-# end
-# array
-# end
-# arr = [3, 1, 2, 4]
-# print sort_bubble(arr, order: :ascending)  # Output: [1, 2, 3, 4]
-# print sort_bubble(arr, order: :descending) # Output: [3, 2, 1, 4]
+end
+end
+end
+break unless swapped
+end
+array
+end
+arr = [3, 1, 2, 4]
+print sort_bubble(arr, order: :ascending)  # Output: [1, 2, 3, 4]
+print sort_bubble(arr, order: :descending) # Output: [3, 2, 1, 4]
